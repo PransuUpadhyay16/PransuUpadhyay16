d13a423e817ce9164ec8fc6880be615136721ca0
PransuUpadhyay16 <116911283+PransuUpadhyay16@users.noreply.github.com>
2022-10-29 12:50:09 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,18 @@
+//SYNTAX OF FOR LOOP
+//for(initialization;test expression;update statement)
+//{
+//   body of the loop
+//}
+//FOR EXAMPLE
+
+
+public class loop {
+    public static void main(String[] args){
+        int i;
+        for(i=1;i<=10;i++)
+        {
+            System.out.println(i);
+
+        }
+    }
+}
